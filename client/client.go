feda319d6e9e2806dcbd5792f87ddb3d10f0aa51
Otabek Doniyorov <otabek2770530@gmail.com>
2021-04-16 16:14:37 -0700

    second commit

--- client/client.go
+++ client/client.go
@@ -19,77 +19,94 @@ type AuthInfo struct {
 	Username, Password string
 }
 
+// get post put delete
 func create(w http.ResponseWriter, r *http.Request) {
-	jsonbody, err := ioutil.ReadAll(r.Body)
-	errHandler("read the response body", err)
-	user := new(User)
-	err = json.Unmarshal(jsonbody, &user)
-	errHandler("unmarshall the response body", err)
-	log.Print(user)
-	conn, err := grpc.Dial(":9090", grpc.WithInsecure())
-	errHandler("establish connection with server", err)
-	client, ctx := pb.NewCrudClient(conn), context.Background()
-	response, err := client.Create(ctx, &pb.UserInfo{
-		Info:      &pb.PrivateUserInfo{Username: user.Username, Password: user.Password},
-		Firstname: user.Firstname,
-		Lastname:  user.Lastname,
-	})
-	errHandler("connect with the grpc server", err)
-	webresponse, err := json.Marshal(response)
-	errHandler("marshaling the response form grpc server", err)
-	w.Write(webresponse)
+	if r.Method == "POST" {
+		jsonbody, err := ioutil.ReadAll(r.Body)
+		errHandler("read the response body", err)
+		user := new(User)
+		err = json.Unmarshal(jsonbody, &user)
+		errHandler("unmarshall the response body", err)
+		log.Print(user)
+		conn, err := grpc.Dial(":9090", grpc.WithInsecure())
+		errHandler("establish connection with server", err)
+		client, ctx := pb.NewCrudClient(conn), context.Background()
+		response, err := client.Create(ctx, &pb.UserInfo{
+			Info:      &pb.PrivateUserInfo{Username: user.Username, Password: user.Password},
+			Firstname: user.Firstname,
+			Lastname:  user.Lastname,
+		})
+		errHandler("connect with the grpc server", err)
+		webresponse, err := json.Marshal(response)
+		errHandler("marshaling the response form grpc server", err)
+		w.Write(webresponse)
+	} else {
+		w.Write([]byte("405 Method not allowed"))
+	}
 }
 
 func read(w http.ResponseWriter, r *http.Request) {
-	jsonbody, err := ioutil.ReadAll(r.Body)
-	errHandler("read the response body", err)
-	user := new(AuthInfo)
-	err = json.Unmarshal(jsonbody, &user)
-	errHandler("unmarshall the response body", err)
-	conn, err := grpc.Dial(":9090", grpc.WithInsecure())
-	errHandler("establish connection with server", err)
-	client, ctx := pb.NewCrudClient(conn), context.Background()
-	response, err := client.Get(ctx, &pb.PrivateUserInfo{Username: user.Username, Password: user.Password})
-	errHandler("connect with the grpc server", err)
-	webresponse, err := json.Marshal(response)
-	errHandler("marshal the response form grpc server", err)
-	w.Write(webresponse)
+	if r.Method == "GET" {
+		jsonbody, err := ioutil.ReadAll(r.Body)
+		errHandler("read the response body", err)
+		user := new(AuthInfo)
+		err = json.Unmarshal(jsonbody, &user)
+		errHandler("unmarshall the response body", err)
+		conn, err := grpc.Dial(":9090", grpc.WithInsecure())
+		errHandler("establish connection with server", err)
+		client, ctx := pb.NewCrudClient(conn), context.Background()
+		response, err := client.Get(ctx, &pb.PrivateUserInfo{Username: user.Username, Password: user.Password})
+		errHandler("connect with the grpc server", err)
+		webresponse, err := json.Marshal(response)
+		errHandler("marshal the response form grpc server", err)
+		w.Write(webresponse)
+	} else {
+		w.Write([]byte("405 Method not allowed"))
+	}
 }
 
 func update(w http.ResponseWriter, r *http.Request) {
-	jsonbody, err := ioutil.ReadAll(r.Body)
-	errHandler("read the response body", err)
-	user := new(User)
-	err = json.Unmarshal(jsonbody, &user)
-	errHandler("unmarshall the response body", err)
-	conn, err := grpc.Dial(":9090", grpc.WithInsecure())
-	errHandler("establish connection with server", err)
-	client, ctx := pb.NewCrudClient(conn), context.Background()
-	response, err := client.Update(ctx, &pb.UserInfo{
-		Info:      &pb.PrivateUserInfo{Username: user.Username, Password: user.Password},
-		Firstname: user.Firstname,
-		Lastname:  user.Lastname,
-	})
-	errHandler("connect with the grpc server", err)
-	webresponse, err := json.Marshal(response)
-	errHandler("marshaling the response form grpc server", err)
-	w.Write(webresponse)
+	if r.Method == "PUT" {
+		jsonbody, err := ioutil.ReadAll(r.Body)
+		errHandler("read the response body", err)
+		user := new(User)
+		err = json.Unmarshal(jsonbody, &user)
+		errHandler("unmarshall the response body", err)
+		conn, err := grpc.Dial(":9090", grpc.WithInsecure())
+		errHandler("establish connection with server", err)
+		client, ctx := pb.NewCrudClient(conn), context.Background()
+		response, err := client.Update(ctx, &pb.UserInfo{
+			Info:      &pb.PrivateUserInfo{Username: user.Username, Password: user.Password},
+			Firstname: user.Firstname,
+			Lastname:  user.Lastname,
+		})
+		errHandler("connect with the grpc server", err)
+		webresponse, err := json.Marshal(response)
+		errHandler("marshaling the response form grpc server", err)
+		w.Write(webresponse)
+	} else {
+		w.Write([]byte("405 Method not allowed"))
+	}
 }
 
 func delete(w http.ResponseWriter, r *http.Request) {
-	jsonbody, err := ioutil.ReadAll(r.Body)
-	errHandler("read the response body", err)
-	user := new(AuthInfo)
-	err = json.Unmarshal(jsonbody, &user)
-	errHandler("unmarshall the response body", err)
-	conn, err := grpc.Dial(":9090", grpc.WithInsecure())
-	errHandler("establish connection with server", err)
-	client, ctx := pb.NewCrudClient(conn), context.Background()
-	response, err := client.Delete(ctx, &pb.PrivateUserInfo{Username: user.Username, Password: user.Password})
-	errHandler("connect with the grpc server", err)
-	webresponse, err := json.Marshal(response)
-	errHandler("marshal the response form grpc server", err)
-	w.Write(webresponse)
+	if r.Method == "DELETE" {
+		jsonbody, err := ioutil.ReadAll(r.Body)
+		errHandler("read the response body", err)
+		user := new(AuthInfo)
+		err = json.Unmarshal(jsonbody, &user)
+		errHandler("unmarshall the response body", err)
+		conn, err := grpc.Dial(":9090", grpc.WithInsecure())
+		errHandler("establish connection with server", err)
+		client, ctx := pb.NewCrudClient(conn), context.Background()
+		response, err := client.Delete(ctx, &pb.PrivateUserInfo{Username: user.Username, Password: user.Password})
+		errHandler("connect with the grpc server", err)
+		webresponse, err := json.Marshal(response)
+		errHandler("marshal the response form grpc server", err)
+		w.Write(webresponse)
+	} else {
+		w.Write([]byte("405 Method not allowed"))
+	}
 }
 
 func errHandler(statement string, err error) {
